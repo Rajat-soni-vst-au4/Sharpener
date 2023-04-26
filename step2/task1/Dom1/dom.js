6319bceb4e52d9bde096fb47dcb7e3144d1a4f30
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-04-26 22:29:01 +0530

    first DOM task done

--- step2/task1/Dom1/dom.js
+++ step2/task1/Dom1/dom.js
@@ -6,3 +6,11 @@ var head = document.all[14]
 // console.log(head)
 head.innerHTML = "ADD ITEM";
 head.style.color = "green";
+
+var items = document.getElementsByClassName("list-group-item");
+
+items[2].style.backgroundColor = "green";
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = "bold";
+}
