9e3c1f73cedefda1bb99f69a33e73a825eeeae48
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-04-26 18:41:31 +0530

    first DOM task done

---
+++ step2/task1/Dom1/dom.js
@@ -0,0 +1,8 @@
+//Examine the document object
+var title = document.getElementById("main-header");
+title.style.borderBottom = "4px solid black";
+
+var head = document.all[14]
+// console.log(head)
+head.innerHTML = "ADD ITEM";
+head.style.color = "green";
