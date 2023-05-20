7b42f4fe7558754b0cb15daad164006fe93d6282
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-20 23:52:59 +0530

    prop passed

--- reactmodule/expense/src/components/ExpenseList.js
+++ reactmodule/expense/src/components/ExpenseList.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import  './ExpenseList.css';
 
-function ExpenseList() {
-  const expenseDate = new Date(2021,2,26);
-  const expenseTitle = 'car insurance';
-  const expenseAmount = 284.99;
-  const LocationOfExpenditure = 'Delhi';
+
+function ExpenseList(props) {
+
   return (
-    <div className="expense-item">
-     <div>{expenseDate.toISOString()   }   </div>
-     
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-      <h2>{LocationOfExpenditure}   </h2>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-  </div>
+    </div>
+    
   )
 }
 
--- reactmodule/expense/src/components/Header.js
+++ reactmodule/expense/src/components/Header.js
@@ -2,10 +2,55 @@ import React from 'react'
 import ExpenseList from './ExpenseList'
 
 function Header() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      LocationOfExpenditure: 'Delhi',
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49,  LocationOfExpenditure: 'Delhi', date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: 'Delhi',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      LocationOfExpenditure: 'Delhi',
+      date: new Date(2021, 5, 12),
+    },
+  ];
+ 
   return (
+   
     <>
         <h1>Expense Items</h1>
-        <ExpenseList/>
+        <ExpenseList
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseList>
+      <ExpenseList
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseList>
+      <ExpenseList
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseList>
+      <ExpenseList
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseList>
     </>
   )
 }
