0677478aeb71e6ea8efc465f07a740f68fe537c8
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-20 21:28:12 +0530

    first basic task done

---
+++ reactmodule/expense/src/components/ExpenseList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ExpenseList() {
+  return (
+    <div>
+      <ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movie Rs 200</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ExpenseList
---
+++ reactmodule/expense/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExpenseList from './ExpenseList'
+
+function Header() {
+  return (
+    <>
+        <h1>Expense Items</h1>
+        <ExpenseList/>
+    </>
+  )
+}
+
+export default Header
