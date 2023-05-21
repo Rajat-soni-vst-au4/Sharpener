a14139d5ca0a68e0c86a519f7d1d7ad1b2fcf36e
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-21 17:24:59 +0530

    final changes done

--- reactmodule/expense/src/components/Header.js
+++ reactmodule/expense/src/components/Header.js
@@ -1,62 +1,43 @@
 import React from 'react'
-import ExpenseList from './ExpenseList'
+import ExpensesList from './Expenses/ExpenseList';
 
-function Header() {
+const Header =()=> {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      LocationOfExpenditure: 'Delhi',
     },
-    { id: 'e2', title: 'New TV', amount: 799.49,  LocationOfExpenditure: 'Delhi', date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      LocationOfExpenditure: 'Delhi',
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      LocationOfExpenditure: 'Delhi',
       date: new Date(2021, 5, 12),
     },
   ];
- 
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-   
-    <>
-        <h1>Expense Items</h1>
-        <ExpenseList
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].LocationOfExpenditure}
-      ></ExpenseList>
-      <ExpenseList
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].LocationOfExpenditure}
-      ></ExpenseList>
-      <ExpenseList
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[1].LocationOfExpenditure}
-      ></ExpenseList>
-      <ExpenseList
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[1].LocationOfExpenditure}
-      ></ExpenseList>
-    </>
-  )
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpensesList items={expenses} />
+    </div>
+  );
 }
+
 
 export default Header
