90165dd33bfa0ea53e4b6faaddd9a71373f7ebb6
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-21 23:05:57 +0530

    button with event added

--- reactmodule/expense/src/components/Expenses/ExpenseDetails.js
+++ reactmodule/expense/src/components/Expenses/ExpenseDetails.js
@@ -5,13 +5,19 @@ import Card from '../UI/Card';
 import './ExpenseList.css';
 
 const ExpenseDetails = (props) =>  {
+
+  function hit(){
+    console.log("helo")
+  }
   return (
     <Card className='expense-item'>
     <ExpenseDate date={props.date} />
     <div className='expense-item__description'>
-      <h2>{props.title}</h2>
+      <h2>{props.title} </h2>
+      <h2> {props.location}</h2>
       <div className='expense-item__price'>${props.amount}</div>
     </div>
+      <button onClick={hit}>Delete Expense</button>
   </Card>
     )
 }
