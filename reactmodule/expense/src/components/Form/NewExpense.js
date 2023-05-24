4d6c50922d6d96ffbb976ccc97fafb7a9c0485bc
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-24 21:27:20 +0530

    form updated with css

--- reactmodule/expense/src/components/Form/NewExpense.js
+++ reactmodule/expense/src/components/Form/NewExpense.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
+import "./NewExpense.css";
 
 function NewExpense(props) {
     //save and trigger data
   const [newtitle, setNewtitle] = useState("");
   const [newamount, setNewamount] = useState("");
   const [newdate, setNewdate] = useState("");
+  const [newlocation, setNewlocation]=useState("");
+  
 
-  const titlechangeHandler = (e) => {
+  const titleChangeHandler = (e) => {
     setNewtitle(e.target.value);
   };
-  const amountchangeHandler = (e) => {
+  const amountChangeHandler = (e) => {
     setNewamount(e.target.value);
   };
-  const datechangeHandler = (e) => {
+  const dateChangeHandler = (e) => {
     setNewdate(e.target.value);
   };
+  const locationChangeHandler=(event)=>{
+    setNewlocation(event.target.value);
+}
 
   //save data in a object collenctionData
   function collenction(e) {
@@ -43,24 +49,44 @@ function NewExpense(props) {
   return (
     <>
       <form onSubmit={collenction}>
-        <input
-          type="text"
-          value={newtitle}
-          placeholder="Expense title"
-          onChange={titlechangeHandler}
-        />
-        <input
-          type="Number"
-          value={newamount}
-          placeholder="Amount"
-          onChange={amountchangeHandler}
-        />
-        <input 
-        type="date"
-        value={newdate}
-        onChange={datechangeHandler}
-        />
-        <button type="submit">ADD</button>
+      <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input 
+                type='text' 
+                value={newtitle} 
+                onChange={titleChangeHandler} />
+            </div>
+            <div className='new-expense__control'>
+                <label>Location</label>
+                <input 
+                type='text' 
+                value={newlocation} 
+                onChange={locationChangeHandler} />
+            </div>
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input 
+                type='number'
+                min="0.01" 
+                step="0.01" 
+                value={newamount}
+                onChange={amountChangeHandler}/>
+            </div>
+            <div className='new-expense__control'>
+                <label>Date</label>
+                <input 
+                type='date' 
+                min="2018-01-01" 
+                max="2023-12-31" 
+                value={newdate}
+                onChange={dateChangeHandler}/>
+            </div>
+        </div>
+        <div className='new-expense__actions'>
+            <button type="button" >Cancel</button>
+            <button type="submit">Add Expense</button>
+        </div>
       </form>
     </>
   );
